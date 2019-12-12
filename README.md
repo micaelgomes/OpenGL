d8b6380f6d9332968bc89dfd1df7602df6685551
micaelgoms <micaellgoms@gmail.com>
2019-12-12 00:59:43 -0300

    luz

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 # CP-openGL
 
-Quando a aplicação é iniciada, a camera está dentro do CUBO, pressione W, até ver completamente o CUBO para interagir.
+Para execução da atividade foi construída uma Esfera (porém como não há iluminação não é possível ver a forma da esfera, mas é possível perceber o movimento).
 
-A mecânica consiste em incrementas as componente R.G.B e variar o posicionamento da câmera em X ou Y
+A mecânica consiste em incrementas as componente R.G.B e rotacionar a esferar em torno de um eixo
 
 ## comandos
 
-a -> decrementa em X
-s -> decrementa em Y
-d -> incrementa em X
-w -> incrementa em Y
++ a -> incrementa o canal R e rotaciona em torno de X 
++ s -> incrementa o canal G e rotaciona em torno de X 
++ d -> incrementa o canal B e rotaciona em torno de X
 
-o -> Projeção Ortogonal
-p -> Projeção perspectiva
++ shift + a -> Decrementa o canal R e rotaciona em torno de X, só que em sentido anti-horário 
++ shift + s -> Decrementa o canal G e rotaciona em torno de Y, só que em sentido anti-horário 
++ shift + d -> Decrementa o canal B e rotaciona em torno de Z, só que em sentido anti-horário
 
-esc -> Fecha a aplicação
-
++ esc -> Fecha a aplicação
 ## Compilação e execução
 
 g++ main.c -o exe -lglut -lGLU -lGL && ./exe
--- main.c
+++ main.c
@@ -5,117 +5,98 @@
  */
 
 #include <GL/glut.h>
-#include <stdlib.h> 
 
-#define true 1
-#define false 0
+GLfloat xRotated, yRotated, zRotated;
+GLdouble radius=1;
 
-GLdouble radius=1, side=50, angle, fAspect;
+static float red = 0.0, green = 1.0, blue = 0.0;
 
-int perspective = true;
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f }; 
+ 
+GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+GLfloat high_shininess[] = { 100.0f }; 
 
-GLfloat eyex=40, eyey=260, eyez=100, centrox, centroy, centroz;
+GLfloat none[] = { 0.0f, 0.0f, 0.0f, 0.0f };
 
-static float red = 0.0, green = 0.0, blue = 0.0;
+void display() {
 
-void displayPerspectiva() {
-    glMatrixMode(GL_PROJECTION);
+    // glMatrixMode(GL_MODELVIEW);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
     glLoadIdentity();
-
-    gluPerspective(angle, fAspect, 0.5, 500);
-    glMatrixMode(GL_MODELVIEW);
-    
-    glLoadIdentity();
-    glColor3f(red, green, blue); 
-    
-    gluLookAt(eyex, eyey, eyez, centrox, centroy, centroz, 0.0, 1.0, 0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
+    glTranslatef(0.0,0.0,-5.0);
+    glColor3d(red, green, blue); 
+    glRotatef(xRotated,1.0,0.0,0.0);
+    glRotatef(yRotated,0.0,1.0,0.0);
+    glRotatef(zRotated,0.0,0.0,1.0);
     // glScalef(1.0,1.0,1.0);// scaling transfomation 
-    glutWireCube(side);
+
+    glPushMatrix();
+    glTranslated(0.0,0.0,0.0);
+    glutSolidSphere(radius,50,50);
+    glPopMatrix(); 
     
+    glutSwapBuffers();
+
     glFlush(); 
-}
-
-void displayOrtogonal() {
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-
-    glOrtho(-65.0, 65.0, -65.0, 65.0, -400.0, 400.0);
-    glMatrixMode(GL_MODELVIEW);
-    
-    glLoadIdentity();
-    glColor3f(red, green, blue); 
-    
-    gluLookAt(eyex, eyey, eyez, centrox, centroy, centroz, 0.0, 1.0, 0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // glScalef(1.0,1.0,1.0);// scaling transfomation 
-    glutWireCube(side);
-    
-    glFlush(); 
-}
-
-void display(void){
-    glClear(GL_COLOR_BUFFER_BIT);
-    if(perspective)
-        displayPerspectiva();
-    else
-        displayOrtogonal();
+   
 }
 
 void keyboard (unsigned char key, int x, int y){
   switch (key) {
     case 'a':
-        if(red <= 1.0) red = red + 0.05;
+        if(red < 1.0) red = red + 0.05;
         else red = 0.0;
-
-        eyex -= 10;
-        if(eyex < -340) eyex = 340;
-        
-        glLoadIdentity();
-        gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
-        glutPostRedisplay();
+        if(xRotated < 360.0) xRotated = xRotated + 10.0;
+        else xRotated = 0.0;
         break;
     case 's':
-        if(green <= 1.0) green = green + 0.05;
-        else green = 0.0; 
-        
-        eyey -= 10.0;
-        if(eyey < -260) eyey = 260;
-
-        glLoadIdentity();
-        gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
-        glutPostRedisplay();
+        if(green < 1.0) green = green + 0.05;
+        else green = 0.0;
+        if(yRotated < 360.0) yRotated = yRotated + 10.0;
+        else yRotated = 0.0;
         break;
     case 'd':
-        if(blue <= 1.0) blue = blue + 0.05;
+        if(blue < 1.0) blue = blue + 0.05;
         else blue = 0.0;
-
-        eyex += 10;
-        if(eyex > 340) eyex = -340;
+        if(zRotated < 360.0) zRotated = zRotated + 10.0;
+        else zRotated = 0.0;
+        break;
+    case 'A':
+        if(red > 0.0) red = red - 0.05;
+        else red = 1.0;
+        if(xRotated > 0.0) xRotated = xRotated - 10.0;
+        else xRotated = 360.0;
+        break;
+    case 'S':
+        if(green > 0.0) green = green - 0.05;
+        else green = 1.0;
+        if(yRotated > 0.0) yRotated = yRotated - 10.0;
+        else yRotated = 360.0;
+        break;
+    case 'D':
+        if(blue > 0.0) blue = blue - 0.05;
+        else blue = 1.0;
+        if(zRotated > 0.0) zRotated = zRotated - 10.0;
+        else zRotated = 360.0;
+        break;
+    case 'j':
+        // glDisable(GL_LIGHT0);
         
-        glLoadIdentity();
-        gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
-        glutPostRedisplay();
-        break;
-    case 'w':
-        if(blue <= 1.0) blue = blue + 0.05;
-        else blue = 0.0;
 
-        eyey += 10;
-        if(eyey > 260) eyey = -260;
-    
-        glLoadIdentity();
-        gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
-        glutPostRedisplay();
+        glLightfv(GL_LIGHT0, GL_AMBIENT, none);
+        glMaterialfv(GL_FRONT, GL_AMBIENT, none);
+
+        glEnable(GL_LIGHT0);
         break;
-    case 'p':
-        perspective = true;
+    case 'k':
         break;
-    case 'o':
-        perspective = false;
+    case 'l':
         break;
     case 27:
         exit(0);
@@ -123,37 +104,61 @@ void keyboard (unsigned char key, int x, int y){
     default:
         break;
   }
+  glutPostRedisplay();
 }
 
-void init(void) {
+void init(int x, int y) {
+    if (y == 0 || x == 0) return;   
+    
     glClearColor (1.0, 1.0, 1.0, 1.0);
-    eyex = 0.0;
-	eyey = 0.0;
-	eyez = 5.0;
-	centrox = 0.0;
-	centroy = 0.0;
-	centroz = 0.0;
-    angle = 45;
-}
 
-void reshape(int w, int h){
-    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
-    fAspect = (GLfloat)w/(GLfloat)h;
+    glMatrixMode(GL_PROJECTION);  
+    glLoadIdentity(); 
+    gluPerspective(39.0,(GLdouble)x/(GLdouble)y,0.6,21.0);
     glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity(); 
+    glViewport(0,0,x,y);
+
+    // set Light
+    glClearColor(1,1,1,1);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK); 
+ 
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS); 
+ 
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING); 
+ 
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position); 
+ 
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+
 }
 
 int main (int argc, char **argv) {
     glutInit(&argc, argv); 
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500,500);
-    glutCreateWindow("MyApp");    
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); 
+    glutCreateWindow("MyApp");
+    
+    xRotated = yRotated = zRotated = 30.0;
+    xRotated=43;
+    yRotated=50;
+ 
     glutDisplayFunc(display);
-    init();
     glutKeyboardFunc(keyboard);
-    glutReshapeFunc(reshape);
+    glutReshapeFunc(init);
+    
     glutMainLoop();
+
     return 0;
 }
